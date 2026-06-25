8f059a0a5197a9701796fc7dc005908761d0bb84
Muflih Salda Maulana <mmuflihsalda4@gmail.com>
2026-06-25 19:27:53 +0700

    Add files via upload

---
+++ lab8_vuejs/assets/js/components/About.js
@@ -0,0 +1,14 @@
+const About = {
+  template: `
+        <div class="home-container">
+            <h2>Profil Mahasiswa</h2>
+            <p><strong>Nama:</strong> Muflih Salda Maulana</p>
+            <p><strong>NIM:</strong> 312410527</p>
+            <p><strong>Program Studi:</strong> Teknik Informatika</p>
+            <p><strong>Universitas:</strong> Universitas Pelita Bangsa</p>
+            <div style="margin-top: 15px;">
+                <img src="https://ui-avatars.com/api/?name=Muflih+Salda&background=3152d6&color=fff&size=150" alt="Avatar Profil" style="border-radius: 50%;">
+            </div>
+        </div>
+    `,
+};
---
+++ lab8_vuejs/assets/js/components/Home.js
@@ -0,0 +1,38 @@
+const Home = {
+
+template:`
+
+<div class="card">
+
+<h2>👋 Selamat Datang di Portal Admin Artikel</h2>
+
+<p>
+
+Gunakan aplikasi ini untuk mengelola seluruh data artikel secara aman
+menggunakan REST API CodeIgniter 4 dan Vue.js.
+
+</p>
+
+<br>
+
+<h3>Fitur Aplikasi</h3>
+
+<ul>
+
+<li>Login Administrator</li>
+
+<li>Kelola Artikel (CRUD)</li>
+
+<li>Upload Gambar</li>
+
+<li>REST API CodeIgniter 4</li>
+
+<li>Vue.js Single Page Application</li>
+
+</ul>
+
+</div>
+
+`
+
+}
